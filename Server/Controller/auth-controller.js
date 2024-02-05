c1a4dc76579c8ab43da797ec6cb5e87300fc927e
Sadabrij <sadabrijshah992@gmail.com>
2024-02-05 21:57:46 +0530

    Update auth-controller.js

--- Server/Controller/auth-controller.js
+++ Server/Controller/auth-controller.js
@@ -1,5 +1,6 @@
 
 const express = require("express");
+const User = require("../Model/user");
 
 const home = async(req, res)=>{
 
@@ -19,10 +20,18 @@ const register = async (req, res) => {
 
     try {
 
-        res
-            .status(200)
-            .json({message: req.body})
+        const {username, email, password, phn, isAdmin} = req.body;
+
+        const isExist = await User.findOne({email:email});
+
+        if( isExist ) {
+                return res.status(400).json({message:"user already exit ....."})
+        }
         
+        const created =  await User.create({username:username, email:email, password:password, phn:phn, isAdmin:isAdmin});
+       
+        res.status(200).json({message: "user created ...", username: created.username, password: created.password})
+
     } catch (error) {
         console.log(`Error accurs inside home ${error}`)
     }
