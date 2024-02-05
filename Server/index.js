a00b08adc7a203684c55198d22fb3fed7b40d3d0
Sadabrij <sadabrijshah992@gmail.com>
2024-02-05 21:57:43 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,25 @@
+
+require("dotenv").config();
+
 const express = require("express");
 const { router } = require("./Router/auth-route");
+const connectDB = require("./Utils/db");
+
 const app     = express();
 const PORT    = 3000;
 
-
 app.use(express.json());
+connectDB();
 
 app.use('/', router);
 app.use('/registre', router);
+
 
 app.listen(PORT, ()=>{
 
     console.log(`Server running on post ${PORT}`)
 
 })
+
+
+
